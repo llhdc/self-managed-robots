5742084a1eedd2552d83ae3fcdb31e33c8212a8c
Leonard <leonard.horne@gmail.com>
2017-08-03 14:30:53 -0400

    Convert anonymous function to lambda. Add add user endpoint.

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/data');
 
-router.get('/:id', function(req, res){
+router.get('/:id', (req, res) => {
   User.findOne({ id: parseInt(req.params.id) })
     .then((data) => {
       res.render('profile', data);
     });
+});
+
+router.get('/add', (req, res) => {
+  res.render('addUser')
 })
 
 module.exports = router;
